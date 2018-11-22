eac2fa0811f7ae3bf4a0834fb7ba54255471fa0c
EricHyh <252882326@qq.com>
2018-11-22 16:00:35 +0800

    开发进行中。。。

--- app/src/main/java/com/hyh/prettyskin/demo/activity/SecondActivity.java
+++ app/src/main/java/com/hyh/prettyskin/demo/activity/SecondActivity.java
@@ -18,5 +18,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
     }
 }
