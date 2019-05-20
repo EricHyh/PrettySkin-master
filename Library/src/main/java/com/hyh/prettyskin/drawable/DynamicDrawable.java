4469e02fc37821e9933c92503cfcd4cf2a7cd5b0
EricHyh <252882326@qq.com>
2019-05-20 18:38:35 +0800

    开发进行中。。。

---
+++ Library/src/main/java/com/hyh/prettyskin/drawable/DynamicDrawable.java
@@ -0,0 +1,38 @@
+package com.hyh.prettyskin.drawable;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+/**
+ * @author Administrator
+ * @description
+ * @data 2019/5/20
+ */
+
+public class DynamicDrawable extends Drawable {
+
+    public DynamicDrawable(String attrName) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
