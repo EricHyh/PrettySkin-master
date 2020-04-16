f956423b8d09886d6b7db57534c9b860c4ad1d6c
Eric_He <hyh123147@gmail.com>
2020-04-16 23:05:03 +0800

    ...

---
+++ lib-android-appcompat/src/main/java/com/hyh/prettyskin/sh/AppBarLayoutSH.java
@@ -0,0 +1,104 @@
+package com.hyh.prettyskin.sh;
+
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.design.widget.AppBarLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hyh.prettyskin.AttrValue;
+import com.hyh.prettyskin.utils.AttrValueHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppBarLayoutSH extends ViewGroupSH {
+
+    private final Class mStyleableClass = android.support.design.R.styleable.class;
+
+    private final String mStyleableName = "AppBarLayout";
+
+    private final List<String> mSupportAttrNames = new ArrayList<>();
+
+    {
+        mSupportAttrNames.add("expanded");
+        //mSupportAttrNames.add("elevation");
+        mSupportAttrNames.add("keyboardNavigationCluster");
+        mSupportAttrNames.add("touchscreenBlocksFocus");
+    }
+
+    private TypedArray mTypedArray;
+
+    public AppBarLayoutSH() {
+        Class<AppBarLayout> appBarLayoutClass = AppBarLayout.class;
+    }
+
+    public AppBarLayoutSH(int defStyleAttr) {
+        super(defStyleAttr);
+    }
+
+    public AppBarLayoutSH(int defStyleAttr, int defStyleRes) {
+        super(defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean isSupportAttrName(View view, String attrName) {
+        return mSupportAttrNames.contains(attrName) || super.isSupportAttrName(view, attrName);
+    }
+
+    @Override
+    public void prepareParse(View view, AttributeSet set) {
+        super.prepareParse(view, set);
+        mTypedArray = view.getContext().obtainStyledAttributes(
+                set,
+                android.support.design.R.styleable.AppBarLayout,
+                0,
+                android.support.design.R.style.Widget_Design_AppBarLayout);
+    }
+
+    @Override
+    public AttrValue parse(View view, AttributeSet set, String attrName) {
+        if (mSupportAttrNames.contains(attrName)) {
+            int styleableIndex = AttrValueHelper.getStyleableIndex(mStyleableClass, mStyleableName, attrName);
+            return AttrValueHelper.getAttrValue(view, mTypedArray, styleableIndex);
+        } else {
+            return super.parse(view, set, attrName);
+        }
+    }
+
+    @Override
+    public void finishParse() {
+        super.finishParse();
+        if (mTypedArray != null) {
+            mTypedArray.recycle();
+            mTypedArray = null;
+        }
+    }
+
+    @Override
+    public void replace(View view, String attrName, AttrValue attrValue) {
+        if (mSupportAttrNames.contains(attrName)) {
+            AppBarLayout appBarLayout = (AppBarLayout) view;
+            switch (attrName) {
+                case "expanded": {
+                    appBarLayout.setExpanded(attrValue.getTypedValue(boolean.class, false));
+                    break;
+                }
+                case "keyboardNavigationCluster": {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        appBarLayout.setKeyboardNavigationCluster(attrValue.getTypedValue(boolean.class, false));
+                    }
+                    break;
+                }
+                case "touchscreenBlocksFocus": {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        appBarLayout.setTouchscreenBlocksFocus(attrValue.getTypedValue(boolean.class, false));
+                    }
+                    break;
+                }
+            }
+        } else {
+            super.replace(view, attrName, attrValue);
+        }
+    }
+}
--- lib-android-appcompat/src/main/java/com/hyh/prettyskin/sh/CardViewSH.java
+++ lib-android-appcompat/src/main/java/com/hyh/prettyskin/sh/CardViewSH.java
@@ -1,11 +1,14 @@
 package com.hyh.prettyskin.sh;
 
 import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.hyh.prettyskin.AttrValue;
+import com.hyh.prettyskin.utils.AttrValueHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +21,9 @@ import java.util.List;
 public class CardViewSH extends ViewGroupSH {
 
 
-    private final Class mStyableClass = android.support.v7.cardview.R.styleable.class;
+    private final Class mStyleableClass = android.support.v7.cardview.R.styleable.class;
 
-    private final String mStyableName = "CardView";
-
+    private final String mStyleableName = "CardView";
 
     private final List<String> mSupportAttrNames = new ArrayList<>();
 
@@ -43,11 +45,11 @@ public class CardViewSH extends ViewGroupSH {
         mSupportAttrNames.add("minHeight");
     }
 
+    private TypedArray mTypedArray;
 
     @SuppressLint("PrivateResource")
     public CardViewSH() {
         super(android.support.v7.cardview.R.attr.cardViewStyle);
-        Class<CardView> cardViewClass = CardView.class;
     }
 
     public CardViewSH(int defStyleAttr) {
@@ -66,20 +68,114 @@ public class CardViewSH extends ViewGroupSH {
     @Override
     public void prepareParse(View view, AttributeSet set) {
         super.prepareParse(view, set);
+        mTypedArray = view.getContext().obtainStyledAttributes(
+                set,
+                android.support.v7.cardview.R.styleable.CardView,
+                mDefStyleAttr,
+                mDefStyleRes);
     }
 
     @Override
     public AttrValue parse(View view, AttributeSet set, String attrName) {
-        return super.parse(view, set, attrName);
+        if (mSupportAttrNames.contains(attrName)) {
+            int styleableIndex = AttrValueHelper.getStyleableIndex(mStyleableClass, mStyleableName, attrName);
+            return AttrValueHelper.getAttrValue(view, mTypedArray, styleableIndex);
+        } else {
+            return super.parse(view, set, attrName);
+        }
     }
 
     @Override
     public void finishParse() {
         super.finishParse();
+        if (mTypedArray != null) {
+            mTypedArray.recycle();
+            mTypedArray = null;
+        }
     }
 
     @Override
     public void replace(View view, String attrName, AttrValue attrValue) {
-        super.replace(view, attrName, attrValue);
+        if (mSupportAttrNames.contains(attrName)) {
+            CardView cardView = (CardView) view;
+            switch (attrName) {
+                case "cardBackgroundColor": {
+                    cardView.setCardBackgroundColor(
+                            attrValue.getTypedValue(ColorStateList.class, cardView.getCardBackgroundColor()));
+                    break;
+                }
+                case "cardCornerRadius": {
+                    cardView.setRadius(attrValue.getTypedValue(float.class, 0.0f));
+                    break;
+                }
+                case "cardElevation": {
+                    cardView.setCardElevation(attrValue.getTypedValue(float.class, 0.0f));
+                    break;
+                }
+                case "cardMaxElevation": {
+                    cardView.setMaxCardElevation(attrValue.getTypedValue(float.class, 0.0f));
+                    break;
+                }
+                case "cardUseCompatPadding": {
+                    cardView.setUseCompatPadding(attrValue.getTypedValue(boolean.class, false));
+                    break;
+                }
+                case "cardPreventCornerOverlap": {
+                    cardView.setPreventCornerOverlap(attrValue.getTypedValue(boolean.class, true));
+                    break;
+                }
+                case "contentPadding": {
+                    int contentPadding = attrValue.getTypedValue(int.class, 0);
+                    cardView.setContentPadding(contentPadding, contentPadding, contentPadding, contentPadding);
+                    break;
+                }
+                case "contentPaddingLeft": {
+                    int contentPaddingLeft = attrValue.getTypedValue(int.class, 0);
+                    cardView.setContentPadding(
+                            contentPaddingLeft,
+                            cardView.getContentPaddingTop(),
+                            cardView.getContentPaddingRight(),
+                            cardView.getContentPaddingBottom());
+                    break;
+                }
+                case "contentPaddingTop": {
+                    int contentPaddingTop = attrValue.getTypedValue(int.class, 0);
+                    cardView.setContentPadding(
+                            cardView.getContentPaddingLeft(),
+                            contentPaddingTop,
+                            cardView.getContentPaddingRight(),
+                            cardView.getContentPaddingBottom());
+                    break;
+                }
+                case "contentPaddingRight": {
+                    int contentPaddingRight = attrValue.getTypedValue(int.class, 0);
+                    cardView.setContentPadding(
+                            cardView.getContentPaddingLeft(),
+                            cardView.getContentPaddingTop(),
+                            contentPaddingRight,
+                            cardView.getContentPaddingBottom());
+                    break;
+                }
+                case "contentPaddingBottom": {
+                    int contentPaddingBottom = attrValue.getTypedValue(int.class, 0);
+                    cardView.setContentPadding(
+                            cardView.getContentPaddingLeft(),
+                            cardView.getContentPaddingTop(),
+                            cardView.getContentPaddingRight(),
+                            contentPaddingBottom);
+                    break;
+                }
+                case "minWidth": {
+                    cardView.setMinimumWidth(attrValue.getTypedValue(int.class, 0));
+                    break;
+                }
+                case "minHeight": {
+                    cardView.setMinimumHeight(attrValue.getTypedValue(int.class, 0));
+                    break;
+                }
+            }
+        } else {
+            super.replace(view, attrName, attrValue);
+        }
     }
 }
